7e659fc306d265b0dc5038b248875604975e3ffe
Haider <haider.salar@gmail.com>
2023-04-11 23:44:05 +0530

    fixes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,10 @@
-import Image from 'next/image'
 import { Lato } from 'next/font/google'
 import Main from '@/components/mainLayout/main'
-import { Metadata } from 'next';
 import Head from 'next/head';
 
 const lato = Lato({ subsets: ['latin'], weight:['700']})
 
-export const metadata = {
-  title: 'Suggest AI',
-  description: 'Made by Ali Haider Khan',
-};
+
 
 
 export default function Home() {
